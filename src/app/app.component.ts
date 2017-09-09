b00ee1a18ba1c8be1f9f99d335787baa969cf0d3
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-09-10 00:08:36 +0500

    refactor: services in 2 column with icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,34 @@ import { Component, OnInit } from '@angular/core';
 
 declare var particlesJS: any;
 
+export interface IServices {
+  icon: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
   title = 'app';
   isActive = false;
+  services: IServices[] = [];
 
   ngOnInit() {
     particlesJS.load('particles-js', 'assets/particles.json', null);
+    this.services.push(
+      {icon: 'assistive-listening-systems', title: 'Advanced Driver Assistance Systems'},
+      {icon: 'play', title: 'In-Vehicle Infotainment'},
+      {icon: 'code', title: 'Vehicle Software Development'},
+      {icon: 'server', title: 'Autonomous Driving Addons'},
+      {icon: 'car', title: 'Car As A Service'},
+      // {icon: 'cubes', title: 'Smart Manufacturing'},
+      {icon: 'database', title: 'Vehicle Data Collection'},
+      {icon: 'bar-chart', title: 'Data Analytics'},
+      {icon: 'tags', title: 'Automated Data Labelling'}
+    );
   }
 }
